17d0ff315f4dcd4668c24ff143deb88abaa2046b
JeremyStubbs <jeremyb.stubbs@gmail.com>
2021-12-20 16:11:14 -0500

    put into class

--- script.js
+++ script.js
@@ -4,97 +4,130 @@ const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = (canvas.width = 600);
 const CANVAS_HEIGHT = (canvas.height = 600);
 
-const playerImage = new Image();
-playerImage.src = 'dwarf.png';
-const playerSpriteWidth = 32;
-const playerSpriteHeight = 32;
-let playerX = 220;
-let playerY = 220;
-let playerVector = 0;
 let playerGameFrame = 8;
-let playerState = 'idleleft';
-const playerStaggerFrames = 8;
-const playerSpriteAnimations = [];
-const playerAnimationStates = [
-	{
-		name: 'idleright',
-		frames: 1,
-	},
-	{
-		name: 'walkright',
-		frames: 8,
-	},
-	{
-		name: 'attackright',
-		frames: 7,
-	},
-	{
-		name: 'useless',
-		frames: 1,
-	},
-	{
-		name: 'dieright',
-		frames: 7,
-	},
-	{
-		name: 'idleleft',
-		frames: 1,
-	},
-	{
-		name: 'walkleft',
-		frames: 8,
-	},
-	{
-		name: 'attackleft',
-		frames: 7,
-	},
-	{
-		name: 'useless',
-		frames: 1,
-	},
-	{
-		name: 'dieleft',
-		frames: 7,
-	},
-];
-playerAnimationStates.forEach((state, index) => {
-	let frames = {
-		loc: [],
-	};
-	for (let j = 0; j < state.frames; j++) {
-		let positionX = j * playerSpriteWidth;
-		let positionY = index * playerSpriteHeight;
-		frames.loc.push({ x: positionX, y: positionY });
-	}
-	playerSpriteAnimations[state.name] = frames;
-});
 
-// console.log(playerSpriteAnimations);
-function playeranimate() {
-	ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-	let position =
-		Math.floor(playerGameFrame / playerStaggerFrames) %
-		playerSpriteAnimations[playerState].loc.length;
-	// console.log(playerGameFrame)
-	let playerFrameX = playerSpriteWidth * position;
-	let playerFrameY = playerSpriteAnimations[playerState].loc[position].y;
-
-	ctx.drawImage(
-		playerImage,
-		playerFrameX,
-		playerFrameY,
+class Character {
+	constructor(
+		imgSRC,
 		playerSpriteWidth,
 		playerSpriteHeight,
 		playerX,
 		playerY,
-		playerSpriteWidth,
-		playerSpriteHeight
+		playerVector,
+		playerState,
+		playerStaggerFrames,
+		playerSpriteAnimations,
+		playerAnimationStates
+	) {
+		this.playerImage = new Image();
+		this.playerImage.src = imgSRC;
+		this.playerSpriteWidth = playerSpriteWidth;
+		this.playerSpriteHeight = playerSpriteHeight;
+		this.playerX = playerX;
+		this.playerY = playerY;
+		this.playerVector = playerVector;
+		this.playerState = playerState;
+		this.playerStaggerFrames = playerStaggerFrames;
+		this.playerSpriteAnimations = playerSpriteAnimations;
+		this.playerAnimationStates = playerAnimationStates;
+	}
+	initialize() {
+		this.playerAnimationStates.forEach((state, index) => {
+			let frames = {
+				loc: [],
+			};
+			for (let j = 0; j < state.frames; j++) {
+				let positionX = j * this.playerSpriteWidth;
+				let positionY = index * this.playerSpriteHeight;
+				frames.loc.push({ x: positionX, y: positionY });
+			}
+			this.playerSpriteAnimations[state.name] = frames;
+		});
+	}
+}
+
+const player = new Character(
+	'dwarf.png',
+	64,
+	64,
+	220,
+	220,
+	'right',
+	'idleleft',
+	8,
+	[],
+	[
+		{
+			name: 'idleright',
+			frames: 1,
+		},
+		{
+			name: 'walkright',
+			frames: 8,
+		},
+		{
+			name: 'attackright',
+			frames: 7,
+		},
+		{
+			name: 'useless',
+			frames: 1,
+		},
+		{
+			name: 'dieright',
+			frames: 7,
+		},
+		{
+			name: 'idleleft',
+			frames: 1,
+		},
+		{
+			name: 'walkleft',
+			frames: 8,
+		},
+		{
+			name: 'attackleft',
+			frames: 7,
+		},
+		{
+			name: 'useless',
+			frames: 1,
+		},
+		{
+			name: 'dieleft',
+			frames: 7,
+		},
+	]
+);
+
+player.initialize();
+
+function characterAnimate() {
+	ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+	let position =
+		Math.floor(playerGameFrame / player.playerStaggerFrames) %
+		player.playerSpriteAnimations[player.playerState].loc.length;
+	let playerFrameX = player.playerSpriteWidth * position;
+	let playerFrameY =
+		player.playerSpriteAnimations[player.playerState].loc[position].y;
+
+	ctx.drawImage(
+		player.playerImage,
+		playerFrameX,
+		playerFrameY,
+		player.playerSpriteWidth,
+		player.playerSpriteHeight,
+		player.playerX,
+		player.playerY,
+		player.playerSpriteWidth,
+		player.playerSpriteHeight
 	);
 
 	playerGameFrame++;
-	requestAnimationFrame(playeranimate);
+	requestAnimationFrame(characterAnimate);
 }
-playeranimate();
+
+characterAnimate();
 
 window.addEventListener(
 	'keydown',
@@ -103,67 +136,67 @@ window.addEventListener(
 			return; // Do nothing if the event was already processed
 		}
 		if (event.key == 'w') {
-			playerY -= 10;
-			if (playerVector == 'left') {
-				playerState = 'walkleft';
+			player.playerY -= 10;
+			if (player.playerVector == 'left') {
+				player.playerState = 'walkleft';
 			}
-			if (playerVector == 'right') {
-				playerState = 'walkright';
+			if (player.playerVector == 'right') {
+				player.playerState = 'walkright';
 			}
 		}
 		if (event.key == 's') {
-			playerY += 10;
-			if (playerVector == 'left') {
-				playerState = 'walkleft';
+			player.playerY += 10;
+			if (player.playerVector == 'left') {
+				player.playerState = 'walkleft';
 			}
-			if (playerVector == 'right') {
-				playerState = 'walkright';
+			if (player.playerVector == 'right') {
+				player.playerState = 'walkright';
 			}
 		}
 		if (event.key == 'a') {
-			playerX -= 10;
-			playerState = 'walkleft';
-			playerVector = 'left';
+			player.playerX -= 10;
+			player.playerState = 'walkleft';
+			player.playerVector = 'left';
 		}
 		if (event.key == 'd') {
-			playerX += 10;
-			playerState = 'walkright';
-			playerVector = 'right';
+			player.playerX += 10;
+			player.playerState = 'walkright';
+			player.playerVector = 'right';
 		}
 		if (event.key == 'W') {
-			playerY -= 10;
-			if (playerVector == 'left') {
-				playerState = 'walkleft';
+			player.playerY -= 10;
+			if (player.playerVector == 'left') {
+				player.playerState = 'walkleft';
 			}
-			if (playerVector == 'right') {
-				playerState = 'walkright';
+			if (player.playerVector == 'right') {
+				player.playerState = 'walkright';
 			}
 		}
 		if (event.key == 'S') {
-			playerY += 10;
-			if (playerVector == 'left') {
-				playerState = 'walkleft';
+			player.playerY += 10;
+			if (player.playerVector == 'left') {
+				player.playerState = 'walkleft';
 			}
-			if (playerVector == 'right') {
-				playerState = 'walkright';
+			if (player.playerVector == 'right') {
+				player.playerState = 'walkright';
 			}
 		}
 		if (event.key == 'A') {
-			playerX -= 10;
-			playerState = 'walkleft';
-			playerVector = 'left';
+			player.playerX -= 10;
+			player.playerState = 'walkleft';
+			player.playerVector = 'left';
 		}
 		if (event.key == 'D') {
-			playerX += 10;
-			playerState = 'walkright';
-			playerVector = 'left';
+			player.playerX += 10;
+			player.playerState = 'walkright';
+			player.playerVector = 'left';
 		}
 		if (event.key == 'Enter') {
-			if (playerVector == 'left') {
-				playerState = 'attackleft';
+			if (player.playerVector == 'left') {
+				player.playerState = 'attackleft';
 			}
-			if (playerVector == 'right') {
-				playerState = 'attackright';
+			if (player.playerVector == 'right') {
+				player.playerState = 'attackright';
 			}
 		}
 		// Cancel the default action to avoid it being handled twice
@@ -173,10 +206,10 @@ window.addEventListener(
 );
 
 window.addEventListener('keyup', function (event) {
-	if (playerVector == 'left') {
-		playerState = 'idleleft';
+	if (player.playerVector == 'left') {
+		player.playerState = 'idleleft';
 	}
-	if (playerVector == 'right') {
-		playerState = 'idleright';
+	if (player.playerVector == 'right') {
+		player.playerState = 'idleright';
 	}
 });
